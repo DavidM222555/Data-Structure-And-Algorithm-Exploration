963726b51317206cab60a53033619a2e9e7e4604
David Martin <somethingcrazy145@gmail.com>
2021-12-19 21:28:30 -0600

    Added graph.py and WeightedGraph.py. Also had Digraph.py inherit from Graph

--- Graph.py
+++ Graph.py
@@ -1,26 +1,24 @@
 from collections import defaultdict
 from typing import List
-from Graph import Graph
 
-class Digraph(Graph):
+class Graph():
+    def __init__(self, list_of_vertices : List[str]):
+        self.dict_for_vertices = defaultdict()
+
+        for vertice in list_of_vertices:
+            self.add_vertice(vertice)
+
     def add_edge(self, a : str, b : str):
         if(a not in self.dict_for_vertices.keys() or b not in self.dict_for_vertices.keys()):
             raise ValueError("At least one vertice is undefined")
 
         self.dict_for_vertices[a].append(b) 
+        self.dict_for_vertices[b].append(a)
 
     # Adds multiple edges at once in the form of [["A", "B"], ["C", "D"]]
     def add_edges(self, list_of_edges : List[List[str]]):
         for edge in list_of_edges:
             self.add_edge(edge[0], edge[1])
-
-    # Adds an edge from a to b and b to a
-    def add_dual_edge(self, a : str, b : str):
-        if(a not in self.dict_for_vertices.keys() or b not in self.dict_for_vertices.keys()):
-            raise ValueError("At least one vertice is undefined")
-        
-        self.add_edge(a, b)
-        self.add_edge(b, a)
 
     def add_vertice(self, vertice_name : str):
         if(vertice_name in self.dict_for_vertices.keys()):
