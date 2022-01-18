7da3582c7de3b8afcdf3ffd8e2e0d74194e37589
David Martin <somethingcrazy145@gmail.com>
2022-01-17 22:38:46 -0600

    Modified comments for DisjointSet.py

--- Graphs/DisjointSet.py
+++ Graphs/DisjointSet.py
@@ -14,6 +14,7 @@ class DisjointSet():
         self.node_to_label_dict = {}
         self.label_to_node_dict = {}
 
+    # Creates a new set with a DisjointSetNet(set_label) as the root node
     def make_set(self, set_label : str):
         if(set_label not in self.list_of_node_names):
             set_to_add = DisjointSetNode(set_label)
@@ -24,7 +25,8 @@ class DisjointSet():
             self.list_of_node_names.append(set_label)
             self.node_to_label_dict[set_to_add] = set_label
             self.label_to_node_dict[set_label] = set_to_add
-        
+    
+    # Finds the root (and thus label) for a given label x in our disjoint-set
     def find(self, x : str):
         return self.find_helper(self.label_to_node_dict[x])
 
@@ -35,6 +37,8 @@ class DisjointSet():
             x.parent = self.find_helper(x.parent)
             return x.parent
 
+    # Merges two disjoint-sets together by merging the two trees into one. The root of the tree is decided by which
+    # has the larger rank
     def merge(self, x : str, y : str):
         root_of_x = self.find(x)
         root_of_y = self.find(y)
@@ -43,7 +47,7 @@ class DisjointSet():
             root_of_y.parent = root_of_x
         elif(root_of_x.rank < root_of_y.rank):
             root_of_x.parent = root_of_y 
-        elif(root_of_x != root_of_y):
+        elif(root_of_x != root_of_y): # The case where they have the same rank and are not the exact same node
             root_of_y.parent = root_of_x
             root_of_x.rank = root_of_x.rank + 1
 
